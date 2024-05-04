c0d919c6f5401282f8fcdbbaace4a0bb69a6c1bc
CleNath3010 <clenathanael@gmail.com>
2024-05-04 15:31:11 +0700

    change deck and sun

--- src/Assets/Player/Deck.java
+++ src/Assets/Player/Deck.java
@@ -1,4 +1,8 @@
 public class Deck{
-    private Plant[] plants;
+    private Plant[] deckOfPlants;
+
+    public Deck() {
+        deckOfPlants = new Plant[6];
+    }
 
 }
--- src/Assets/Player/Sun.java
+++ src/Assets/Player/Sun.java
@@ -1,11 +1,17 @@
 public class Sun{
     static int sun;
 
-    public int getSun(){
+    public static int getSun(){
         return sun;
     }
 
-    public void reduceSun(int cost){
+    public static void reduceSun(int cost){
         sun -= cost;
     }
+
+    public static void increaseSun (int cost) {
+        sun += cost;
+    }
+
+    // TO DO: Buat generate sun tiap berapa second
 }
