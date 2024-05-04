a22a5d783732784470a7bb3d0c5bef938ed14078
CleNath3010 <clenathanael@gmail.com>
2024-05-04 15:56:30 +0700

    inventory bingung

--- src/Assets/Player/Inventory.java
+++ src/Assets/Player/Inventory.java
@@ -1,7 +1,56 @@
-import java.util.ArrayList;
+
 
 public class Inventory{
-    ArrayList<Plant> InventoryTanaman;
+    // ArrayList<Plant> InventoryTanaman;
+
+    // public void printListTanaman(){
+    //     for (Plant plant : InventoryTanaman){
+    //         System.out.println(plant.getName());
+    //     }
+    // }
+
+    // public void switchListTanaman (Plant plant1, Plant plant2){
+    //     int index1 = InventoryTanaman.indexOf(plant1);
+    //     int index2 = InventoryTanaman.indexOf(plant2);
+    //     InventoryTanaman.set(index1, plant2);
+    //     InventoryTanaman.set(index2, plant1);
+    // }
+
+    // public void addTanamanToDeck (Plant plant) {
+
+    // }
+    
+    private Plant [] plant_inventory;
+    private Deck deckOfPlants;
+
+    public Inventory(){
+        plant_inventory = new Plant[6];
+        deckOfPlants = new Deck();
+    }
+
+    public void switchInventoryTanaman (int indeks1, int indeks2) {
+        Plant temp = plant_inventory[indeks1];
+        plant_inventory[indeks1] = plant_inventory[indeks2];
+        plant_inventory[indeks2] = temp;
+    }
+
+    public Plant [] getInventory() {
+        return plant_inventory;
+    }
+
+    public Deck getDeck() {
+        return deckOfPlants;
+    }
+
+    // public void addPlantToDeck (Plant plant, int indeks) {
+
+    // }
+
+    // public void removePlantFromDeck (int indeks) { // tambahin exception
+    //     deckOfPlants[indeks] = null;
+    // }
+
+
 
     
 }
