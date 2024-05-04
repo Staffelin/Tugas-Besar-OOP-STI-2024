0b3327083eb3df7d0047679dc473aa90629dbaac
CleNath3010 <clenathanael@gmail.com>
2024-05-04 17:54:17 +0700

    change inventory type to arraylist

--- src/Assets/Player/Inventory.java
+++ src/Assets/Player/Inventory.java
@@ -1,5 +1,5 @@
 package Player;
-
+import java.util.ArrayList;
 import Plants.*;
 
 public class Inventory{
@@ -22,28 +22,27 @@ public class Inventory{
 
     // }
     
-    private Plant [] plant_inventory;
+    private ArrayList <Plant> plant_inventory;
     private Deck deckOfPlants;
 
     public Inventory(){
-        plant_inventory = new Plant[10];
+        plant_inventory = new ArrayList<Plant>();
         deckOfPlants = new Deck();
     }
 
     public void showInventory() {
-        System.out.println(plant_inventory.length);
-        for (int i = 0; i < plant_inventory.length; i++) {
-            System.out.println((i+1) + ". " + plant_inventory[i].getName());
+        for (int i = 0; i < plant_inventory.size(); i++) {
+            System.out.println((i+1) + ". " + plant_inventory.get(i).getName());
         }
     }
 
     public void switchInventoryTanaman (int indeks1, int indeks2) {
-        Plant temp = plant_inventory[indeks1];
-        plant_inventory[indeks1] = plant_inventory[indeks2];
-        plant_inventory[indeks2] = temp;
+        Plant temp = plant_inventory.get(indeks1);
+        plant_inventory.set(indeks1, plant_inventory.get(indeks2));
+        plant_inventory.set(indeks2, temp);
     }
 
-    public Plant [] getInventory() {
+    public ArrayList<Plant> getInventory() {
         return plant_inventory;
     }
 
@@ -62,16 +61,13 @@ public class Inventory{
     }
 
     public void addPlant (int indeks) {
-        deckOfPlants.getDeckOfPlants().add(plant_inventory[indeks]);
+        deckOfPlants.getDeckOfPlants().add(plant_inventory.get(indeks));
     }
 
-    // public void addPlantToDeck (Plant plant, int indeks) {
 
-    // }
-
-    // public void removePlantFromDeck (int indeks) { // tambahin exception
-    //     deckOfPlants[indeks] = null;
-    // }
+    public void removePlantFromDeck (int indeks) { // tambahin exception
+        deckOfPlants.getDeckOfPlants().remove(indeks);
+    }
 
 
 
