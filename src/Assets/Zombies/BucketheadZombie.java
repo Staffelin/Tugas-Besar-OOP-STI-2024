7d836752c65a465ed4b0bc1e399804a79d56318d
devacarissa <devacarissa@gmail.com>
2024-05-09 22:28:45 +0700

    metal

--- src/Assets/Zombies/BucketheadZombie.java
+++ src/Assets/Zombies/BucketheadZombie.java
@@ -4,4 +4,8 @@ public class BucketheadZombie extends Zombie {
     public BucketheadZombie () {
         super("Buckethead Zombie", 300, 100, 1, 1, false);
     }
+
+    public void setHasMetal(has_metal : boolean) {
+        this.has_metal = has_metal;
+    }
 }
--- src/Assets/Zombies/FootballZombie.java
+++ src/Assets/Zombies/FootballZombie.java
@@ -4,4 +4,8 @@ public class FootballZombie extends Zombie {
     public FootballZombie () {
         super("Football Zombie", 300, 100, 1, 4, false);
     }
+
+    public void setHasMetal(has_metal : boolean) {
+        this.has_metal = has_metal;
+    }
 }
