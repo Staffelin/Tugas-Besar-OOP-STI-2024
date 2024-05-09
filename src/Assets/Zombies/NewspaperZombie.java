3b5839176f5ccc16085eed255a9a5e4576aa3940
devacarissa <devacarissa@gmail.com>
2024-05-09 22:19:58 +0700

    speed

--- src/Assets/Zombies/NewspaperZombie.java
+++ src/Assets/Zombies/NewspaperZombie.java
@@ -4,4 +4,12 @@ public class NewspaperZombie extends Zombie {
     public NewspaperZombie () {
         super("Newspaper Zombie", 175, 100, 1, 5, false);
     }
+
+    public increaseSpeed() {
+        if (this.health <= 100) {
+            this.currentSpeed = 3;
+        }
+    }
+
 }
+
