912175b175d70e3bf478254ac05f5acb84b677d7
M Raihan Ariffianto <52344545+Staffelin@users.noreply.github.com>
2024-05-17 19:07:48 +0700

    Update Map.java

--- src/Assets/Map/Map.java
+++ src/Assets/Map/Map.java
@@ -176,22 +176,18 @@ public class Map {
         for (int i = 0; i < 6; i++) {
             for(int j = 1; j < 10; j++){
                 Petak currTile = getFromMatriksPetak(i, j);
-                if(currTile instanceof PetakDarat && currTile.getListZombies().size() > 0){
-                    if(currTile.getListTanaman().size() > 0){
-                        Plant currPlant = currTile.getListTanaman().get(0);
+                if(currTile instanceof PetakDarat && currTile.getListTanaman().size() > 0){
+                    Plant currPlant = currTile.getListTanaman().get(0);
+                    currPlant.attack();
+                    if(currPlant != null){
                         currPlant.attack();
-                        if(currPlant != null){
-                            currPlant.attack();
-                        }
                     }
                 }
-                else if(currTile instanceof PetakKolam && currTile.getListZombies().size() > 0){
-                    if(currTile.getListTanaman().size() > 0){
-                        Plant currPlant = currTile.getListTanaman().get(0);
+                else if(currTile instanceof PetakKolam && currTile.getListTanaman().size() == 2){
+                    Plant currPlant = currTile.getListTanaman().get(1);
+                    currPlant.attack();
+                    if(currPlant != null){
                         currPlant.attack();
-                        if(currPlant != null){
-                            currPlant.attack();
-                        }
                     }
                 }
             }
